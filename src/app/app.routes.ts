da777b9e36008e069a0e7ae4757325de08734044
Rafael <rafael.constantino89@gmail.com>
2024-03-20 20:19:50 -0300

    feat: initial configs and assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PatientsComponent } from './components/patients/patients.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'patients', component: PatientsComponent },
+];
